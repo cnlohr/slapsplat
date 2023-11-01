688470283cd46031590f19c880005338d2725efb
CNLohr <lohr85@gmail.com>
2023-10-31 22:35:13 -0400

    Update with triggers.

---
+++ Assets/Scripts/TriggerArrival.cs
@@ -0,0 +1,22 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class TriggerArrival : UdonSharpBehaviour
+{
+	public GameObject ToggleOff0;
+	public GameObject ToggleOff1;
+	public GameObject ToggleOn0;
+
+    void Start()
+    {
+        
+    }
+	public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+	{
+		ToggleOff0.SetActive(false);
+		ToggleOff1.SetActive(false);
+		ToggleOn0.SetActive(true);
+	}
+}
---
+++ Assets/Scripts/WarpTo.cs
@@ -0,0 +1,33 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class WarpTo : UdonSharpBehaviour
+{
+	public GameObject Destination;
+	
+	public GameObject ToggleOff0;
+	public GameObject ToggleOff1;
+
+	public GameObject ToggleOn0;
+
+    void Start()
+    {
+        
+    }
+	public override void Interact()
+	{
+		ToggleOff0.SetActive(false);
+		ToggleOff1.SetActive(false);
+		ToggleOn0.SetActive(true);
+	
+        Networking.LocalPlayer.TeleportTo(Destination.transform.position, 
+                                          Destination.transform.rotation, 
+                                          VRC_SceneDescriptor.SpawnOrientation.Default, 
+                                          false);
+										  
+										  
+	}
+}
